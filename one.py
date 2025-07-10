2fdbd545eeb7c9e31debbb8344ba55fbf0a5b6d2
Keshavamurti Vadavi <keshavamurti.v@brillio.com>
2025-07-10 17:47:58 +0530

    Second Commit

---
+++ one.py
@@ -0,0 +1,9 @@
+netstat -an | findstr :135
+sc query winmgmt
+nltest /sc_verify:coatsad.com
+nltest /dsgetdc:brillio.com
+net Config workstation
+ADBDGPW10IOTS01	Server	10.128.24.70
+virtmgmt.msc
+
+ADMXOZHV04\C$\
